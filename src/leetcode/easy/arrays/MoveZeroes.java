689a3e75c235c6f24187ad58fd6dfdeb4a38f9a0
Arup K <good.arup@gmail.com>
2023-10-08 07:41:18 -0700

    movezeroes leetcode

--- src/leetcode/easy/arrays/MoveZeroes.java
+++ src/leetcode/easy/arrays/MoveZeroes.java
@@ -9,9 +9,11 @@ public class MoveZeroes {
             int end = len-1;
             for(int i=0;i<nums.length;i++){
                 if(nums[i]==0 && i<end){
-                    nums = shiftBy1(nums,i,end);
-                    nums[end] = 0;
-                    end--;
+                    if(nums[i+1]!=0) {
+                        nums = shiftBy1(nums, i, end);
+                        nums[end] = 0;
+                        end--;
+                    }
                 }
 
             }
@@ -20,13 +22,13 @@ public class MoveZeroes {
 
     private static int[] shiftBy1(int[] a,int src, int end) {
         for(int i=src;i<end;i++){
-            a[i]=a[i+1];
+                a[i]=a[i+1];
         }
         return a;
     }
 
     public static void main(String[] args) {
-        int [] nums = {0};
+        int [] nums = {0,0,1};
         int [] res = moveZeroes(nums);
         Arrays.stream(res).forEach(System.out::println);
     }
